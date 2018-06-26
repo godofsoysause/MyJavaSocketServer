ecfcd903493bdc68331b631632a0ca7e4b13324a
Chencpy6 <253545375@qq.com>
2018-06-26 15:26:23 +0800

    6/26  15:26

--- src/SocketServer.java
+++ src/SocketServer.java
@@ -1,8 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SocketServer {
 
 	public static void main(String[] args) {
-		System.out.println("客户端:");
+		try {
+	         ServerSocket ss = new ServerSocket(8888);
+	         System.out.println("启动服务器....");
+	         System.out.println("服务器:"+ss.getInetAddress().getLocalHost()+"已ok");
+	         Socket s = ss.accept();
+	         System.out.println("客户端:"+s.getInetAddress().getLocalHost()+"已连接到服务器");
+	         
+	         DataInputStream in = new DataInputStream(s.getInputStream());
+	         while(true) {
+             String string = in.readUTF();
+             System.out.println("client:" + string);
+             }
+	         
+	      } catch (IOException e) {
+	         //e.printStackTrace();
+	      }
+		System.out.println("客户端：end");
 	}
 
 }
